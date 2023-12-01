66e2818e6462d3c1ecb76829053afefff7b09517
sayalikandarkar <sayalikandarkar.io@gmail.com>
2023-12-01 18:18:20 -0500

    updating the prompt template file

--- src/prompt_templates.py
+++ src/prompt_templates.py
@@ -25,23 +25,30 @@ class PromptTemplates:
         formatter = FormatSeedPromptData()
         with open(variables_file_path, 'r') as file,  open(seed_messages_variables_output_file, 'w') as seed_messages_variables_output_file:
             variables = [line.strip() for line in file]
-        modified_messages_variables = [message.format(*variables) for message in seed_messages_verbs_output_file]
-        for message in modified_messages_variables:
-            json_object = formatter.line_to_json_02(message)
-            if json_object:
-                seed_messages_variables_output_file.write(json.dumps(json_object) + '\n')
-                return seed_messages_variables_output_file
+            modified_messages_variables = [message.format(*variables) for message in seed_messages_verbs_output_file]
+            print('modified_messages_variables',modified_messages_variables)
+        # for message in modified_messages_variables:
+        #     print('message',message)
+        #     json_object = formatter.line_to_json_seed_data(message)
+        #     if json_object:
+        #         seed_messages_variables_output_file.write(json.dumps(json_object) + '\n')
+        #         return seed_messages_variables_output_file
+            seed_messages_variables_output_file.write(json.dumps(modified_messages_variables) + '\n')
+            return modified_messages_variables
     
     def generate_likert_scales_for_prompts(self, seed_messages_variables_output_file, likert_file_path, prompt_output_file_path):
         formatter = FormatSeedPromptData()
         for message in seed_messages_variables_output_file:
             with open(likert_file_path, 'r') as input_file, open(prompt_output_file_path, 'w') as prompt_output_file:
                 likert_scales = [line.strip() for line in input_file]
-                text = "Instructions: Generate and print 5 versions of the {message}, according to this Likert Scale: [{scale}]"
+                # text = "Instructions: {message} [{scale}]"
+                text = "{message} [{scale}]"
                 #for message in modified_messages_variables:
                 for scale in likert_scales:
                     formatted_prompt = text.format(message=message, scale=scale)
-                    json_object = formatter.line_to_json_02(formatted_prompt)
-                    if json_object:
-                        prompt_output_file.write(json.dumps(json_object) + '\n')
+                    # json_object = formatter.line_to_json_02(formatted_prompt)
+                    # if json_object:
+                    print('formatted_prompt',formatted_prompt)
+                    prompt_output_file.write(json.dumps([formatted_prompt]) + '\n')
+                    return [formatted_prompt]
         
