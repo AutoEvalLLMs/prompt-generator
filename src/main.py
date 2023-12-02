75a00d5ed3c4e2521db34c91f8e706a36cb44cdc
Sara Kingsley <44322187+sarakingsley@users.noreply.github.com>
2023-12-02 16:19:45 -0500

    ugh attempts to fix .py bugs

--- src/main.py
+++ src/main.py
@@ -11,48 +11,41 @@ from open_files import open_file
 #with open_file('output/output_file.jsonl') as f:
     #output_jsonl_file = f.read()
     
-#input_text_file = 'data/seed-data'
+#input_text_file = '/Users/skingsle/llm-class-term-project/prompt-generator/data/seed_variables'
 #output_jsonl_file = 'output/output_file.jsonl'
+
+# formatter = FormatSeedPromptData()
+# formatter.convert_text_to_jsonl('input_file.txt', 'output_file.jsonl')
 
 # convert your seed_data.txt file to a .jsonl file
 #FormatSeedPromptData.convert_text_to_jsonl(input_file_path=input_text_file, output_file_path=output_jsonl_file)
-# formatter = FormatSeedPromptData()
-# formatter.convert_text_to_jsonl(input_text_file, output_jsonl_file)
+#formatter = FormatSeedPromptData()
+#seed_variables = formatter.convert_text_to_jsonl(input_text_file, output_jsonl_file)
 
 # Replace 'your_message_templates.txt' with the path to your actual text file.
 #with open_file('data/seed-data') as f:
     #input_text_file = f.read()
-    
-    
-seed_messages = 'data/seed_messages'  # Replace with the path to your seed-data variables file.
-seed_verbs = 'data/seed_verbs'  # Replace with the path to your seed-data variables file.
-seed_variables = 'data/seed_variables'  # Replace with the path to your seed-data variables file.
-seed_likert_scales = 'data/likert_scale_dict'
 
-#seed_message_output = 'messages_output_file.jsonl'
-
-#variables_file_path = 'output/output_file.jsonl'
-
-#verbs_file_path = 'data/verbs'                         # Replace with the path to your verbs file.
+# define the seed_verbs input and output file paths:
+seed_verbs = '/Users/skingsle/llm-class-term-project/prompt-generator/data/verbs'  # Replace with the path to your seed-data variables file.
+verbs_output_file = 'output/verbs_output_file.jsonl'
 
 
+# define the seed_messages input file path:
+seed_messages = '/Users/skingsle/llm-class-term-project/prompt-generator/data/seed_messages'  # Replace with the path to your seed-data variables file.
 
-#seed_messages_verbs_path = 'output/seed_messages_verbs'
-#seed_messages_variables_path = 'output/seed_messages_variables'
-#seed_messages_variables_output_file = 'output/seed_messages_variables'
-#seed_messages_verbs_output_file=seed_messages_verbs_path
+# convert the seed_variable input TEXT file to a .jsonl file
+input_text_file = '/Users/skingsle/llm-class-term-project/prompt-generator/data/seed_variables'
+output_jsonl_file = 'output/output_file.jsonl'
+formatter = FormatSeedPromptData()
+seed_variables = formatter.convert_text_to_jsonl(input_text_file, output_jsonl_file)
 
-# Replace 'your_likert_scale_text_file.txt' with the path to your actual text file.
-#seed_likert_scales = 'data/likert_scale_dict'
-
-
-#prompt_output_file_path = 'output/prompt_output_file.jsonl'
-
+# define the seed_likert_scales input file path:
+seed_likert_scales = '/Users/skingsle/llm-class-term-project/prompt-generator/data/likert_scale_dict'
 
 # Get prompt TEMPLATES to feed into LLM: 
-
-# initialize PromptTemplates class:
-promptsTemplateGenerator = PromptTemplates(seed_messages, seed_verbs, seed_variables, seed_likert_scales)
+#### initialize PromptTemplates class:
+promptsTemplateGenerator = PromptTemplates(seed_messages, seed_verbs, seed_variables, seed_likert_scales, verbs_output_file)
 
 # generate modified messages with verbs:
 #modified_messages_verbs = promptsTemplateGenerator.modify_message_with_verbs(message_templates, verbs_file_path, seed_messages_verbs_path)
--- src/prompt_templates.py
+++ src/prompt_templates.py
@@ -5,28 +5,26 @@ import json
 
 class PromptTemplates:
     
-    def __init__(self, seed_messages, seed_verbs, seed_variables, seed_likert_scales):
+    def __init__(self, seed_messages, seed_verbs, seed_variables, seed_likert_scales, verbs_output_file):
         self.seed_messages = seed_messages
         self.seed_verbs = seed_verbs
         self.seed_variables = seed_variables
         self.seed_likert_scales = seed_likert_scales
-        #self.likert_file_path = likert_file_path
+        self.verbs_output_file = verbs_output_file
     
-    def modify_message_with_verbs(self, seed_messages, seed_verbs):
-        #formatter = FormatSeedPromptData()
-        with open(seed_verbs, 'r') as verbs_input_file, open(verbs_output_file, 'w') as verbs_output_file:
+    def modify_message_with_verbs(self, seed_messages, seed_verbs, verbs_output_file):
+        formatter = FormatSeedPromptData()
+        seed_verbs = self.seed_verbs
+        with open(seed_verbs, 'r') as seed_verbs, open(verbs_output_file, 'w') as verbs_output_file:
             verbs = [line.strip() for line in seed_verbs]
             modified_messages_verbs = [template.format(*verbs) for template in seed_messages]
             print('modified_messages_verbs', modified_messages_verbs)
-        #for message in modified_messages_verbs:
-            #json_object = formatter.line_to_json_02(message)  # Assuming that this method exists
-            #if json_object:
-            verbs_output_file.write(json.dumps(modified_messages_verbs) + '\n')
-            return modified_messages_verbs
+            verbs_output_file.write(json.dumps([modified_messages_verbs]) + '\n')
+            return [modified_messages_verbs]
     
     def modify_message_with_variables(self, seed_messages, seed_verbs, seed_variables):
-        #formatter = FormatSeedPromptData()
-        modified_messages_verbs = self.modify_message_with_verbs(self, seed_messages, seed_verbs)
+        modified_messages_verbs = self.modify_message_with_verbs(seed_messages, seed_verbs, verbs_output_file)
+        verbs_output_file = 'output/verbs_output_file.jsonl'
         with open(verbs_output_file, 'r') as verbs_output_file,  open(variables_output_file, 'w') as variables_output_file:
             variables = [line.strip() for line in seed_variables]
             modified_messages_variables = [message.format(*variables) for message in modified_messages_verbs]
