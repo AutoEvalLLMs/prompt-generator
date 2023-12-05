79039a6e4f0f176edcc30ad08c2e859815385579
Sara Kingsley <44322187+sarakingsley@users.noreply.github.com>
2023-12-04 19:22:07 -0500

    cleaned up prepare data py

--- src/prepare_seed_prompt_data.py
+++ src/prepare_seed_prompt_data.py
@@ -28,8 +28,6 @@ class FormatSeedPromptData:
         :rtype: dict or None
         """
         parts = line.strip().split('; ')
-        #if len(parts) < 3:
-            #return None
         return {'scale': parts[0]}
 
     
@@ -62,31 +60,6 @@ class FormatSeedPromptData:
                         print(f"Error decoding JSON: {e}, Line: {line}")
                         # Handle the error as required, maybe skip the line or exit
         return variables_list
-    
-    '''
-    def convert_text_to_jsonl(self, input_path, output_path):
-        with open(input_path, 'r') as input_file, open(output_path, 'w') as output_file:
-            variables = [json.loads(line.strip()) for line in input_file]  # Assuming each line is a valid JSON
-            for variable in variables:
-                output_file.write(json.dumps(variable) + '\n')
-        return variables
-
-    def convert_text_to_jsonl(self, input_file_path, output_file_path):
-        """
-        Convert a text file where each line is an entry separated by commas
-        into a JSON Lines (jsonl) file where each line is a JSON object.
-
-        :param input_file_path: The path to the input text file.
-        :type input_file_path: str
-        :param output_file_path: The path to the output jsonl file.
-        :type output_file_path: str
-        """
-        with open(input_file_path, 'r') as text_file, open(output_file_path, 'w') as output_jsonl_file:
-            for line in text_file:
-                json_object = self.line_to_json_seed_data(line)
-                if json_object:
-                    output_jsonl_file.write(json.dumps(json_object) + '\n')
-'''
 
 # Example usage:
 # formatter = FormatSeedPromptData()
